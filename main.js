de4d1543675e18be8f941969f49b74175b1e2efd
VECTOR-DEVRAJ <158202923+VECTOR-DEVRAJ@users.noreply.github.com>
2024-01-30 22:40:13 +0530

    Add files via upload
    html and css

---
+++ main.js
@@ -0,0 +1,16 @@
+// var typed = new typed(".text", {
+//     strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+//     typespeed: 100,
+//     backspeed: 100,
+//     backdelay: 1000,
+//     loop: true
+
+// });
+
+var Typed= new Typed(".text",{
+    strings:["Frontend Developer","Student","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
